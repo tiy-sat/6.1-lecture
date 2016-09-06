dd0baaec9348dfbbd3aabd1de637c9dc337645b3
Jason Vanderslice <jlvanderslice@gmail.com>
2016-09-06 11:34:38 -0500

    Brought in react boilerplate changes for new node react boilerplate integration

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  entry: './build.js',
+  devtool: 'source-map',
+  output: {
+    filename: 'bundle.js',
+    publicPath: 'public/javascripts',
+    path: "public/javascripts"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader?presets[]=es2015&presets[]=react' }
+    ]
+  }
+}
